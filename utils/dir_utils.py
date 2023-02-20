b3ceec3a4377f29e56a19be7279a7d8b87e0f4d0
Sara Fernandez <saradfrz@gmail.com>
2023-02-20 18:50:44 -0300

    feat: create_directory_tree

--- utils/dir_utils.py
+++ utils/dir_utils.py
@@ -5,7 +5,6 @@ def is_dir(mypath):
     return os.path.isdir(mypath)
 
 
-
 def create_dir(mypath):
     if not is_dir(mypath):
         try: 
@@ -13,6 +12,9 @@ def create_dir(mypath):
         except Exception as e:
             debug_log(str(e))
 
-def create_directory_tree(directory_tree):
-    # directory_tree: list of strings
-    pass
+
+def create_directory_tree(dir_list):
+    for d in dir_list:
+        path = os.path.join(*d.split('/')) 
+        if not is_dir(d):
+            os.makedirs(path, exist_ok=True)
