c86629cacdeaaca6afb8a0e673bb5b9ff60a11bf
Sara Fernandez <saradfrz@gmail.com>
2023-02-20 19:01:02 -0300

    fix: project dir tree on startup

--- utils/dir_utils.py
+++ utils/dir_utils.py
@@ -1,5 +1,4 @@
 import os
-from .log_utils import debug_log
 
 def is_dir(mypath):
     return os.path.isdir(mypath)
@@ -7,14 +6,10 @@ def is_dir(mypath):
 
 def create_dir(mypath):
     if not is_dir(mypath):
-        try: 
-            os.mkdir(mypath)
-        except Exception as e:
-            debug_log(str(e))
-
+        os.mkdir(mypath)
 
 def create_directory_tree(dir_list):
     for d in dir_list:
         path = os.path.join(*d.split('/')) 
-        if not is_dir(d):
+        if not is_dir(path):
             os.makedirs(path, exist_ok=True)
