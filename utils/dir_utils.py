4dea7daf7c0d6fcce8b275263580b302f982e74d
Sara Fernandez <saradfrz@gmail.com>
2023-02-20 19:27:25 -0300

    feat: get_files_in_directory

--- utils/dir_utils.py
+++ utils/dir_utils.py
@@ -13,3 +13,10 @@ def create_directory_tree(dir_list):
         path = os.path.join(*d.split('/')) 
         if not is_dir(path):
             os.makedirs(path, exist_ok=True)
+
+def get_files_in_directory(directory_path):
+    files = []
+    for file_name in os.listdir(directory_path):
+        if os.path.isfile(os.path.join(directory_path, file_name)):
+            files.append(file_name)
+    return files
